59d5f618907237a931d2593ce907314550b1e8f6
Fallou-Mbengue <falloumbengue96@gmail.com>
2021-05-05 20:39:45 +0000

    dépot du fichier

---
+++ gestionCohorte.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Versionning_github</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+        echo "Test github";
+        ?>
+    </body>
+</html>
